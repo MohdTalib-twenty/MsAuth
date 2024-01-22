5b93ed0a0bedd3cd413909fc9420161419751a3d
MohdTalib-twenty <abdulanas0000@gmail.com>
2024-01-22 16:15:02 +0530

    Notes Route Added

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const authRoutes = require("./routes/authroutes")
-
+const notesRoute = require("./routes/notesRoutes")
 
 const app = express();
 
 app.use('/api',authRoutes)
+app.use('/api',notesRoute)
 
 
 app.listen(8000,()=>{
